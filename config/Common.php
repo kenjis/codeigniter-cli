df37214841fb916b1ba5c41ff9bf61fb172581d1
kenjis <kenji.uui@gmail.com>
2015-04-28 19:35:31 +0900

    Add copyright comments

--- config/Common.php
+++ config/Common.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Part of Cli for CodeIgniter
+ *
+ * @author     Kenji Suzuki <https://github.com/kenjis>
+ * @license    MIT License
+ * @copyright  2015 Kenji Suzuki
+ * @link       https://github.com/kenjis/codeigniter-cli
+ */
 
 namespace Kenjis\CodeIgniter_Cli\_Config;
 
--- config/_env.php
+++ config/_env.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Part of Cli for CodeIgniter
+ *
+ * @author     Kenji Suzuki <https://github.com/kenjis>
+ * @license    MIT License
+ * @copyright  2015 Kenji Suzuki
+ * @link       https://github.com/kenjis/codeigniter-cli
+ */
 
 // set the mode here only if it is not already set.
 // this allows for setting via shell variables, integration testing, etc.
