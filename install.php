527720424a8c39b45182d5f9679fe5e793134d63
kenjis <kenji.uui@gmail.com>
2015-04-28 11:38:19 +0900

    Add installer script

---
+++ install.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Part of CodeIgniter Cli
+ *
+ * @author     Kenji Suzuki <https://github.com/kenjis>
+ * @license    MIT License
+ * @copyright  2015 Kenji Suzuki
+ * @link       https://github.com/kenjis/codeigniter-cli
+ */
+
+$installer = new Installer();
+$installer->install();
+
+class Installer
+{
+    public static function install()
+    {
+        static::recursiveCopy('vendor/kenjis/codeigniter-cli/config', 'config');
+        
+        @mkdir('tmp', 0755);
+        @mkdir('tmp/log', 0755);
+        
+        copy('vendor/kenjis/codeigniter-cli/cli/console.php', 'console.php');
+    }
+
+    /**
+     * Recursive Copy
+     *
+     * @param string $src
+     * @param string $dst
+     */
+    private static function recursiveCopy($src, $dst)
+    {
+        @mkdir($dst, 0755);
+        
+        $iterator = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($src, \RecursiveDirectoryIterator::SKIP_DOTS),
+            \RecursiveIteratorIterator::SELF_FIRST
+        );
+        
+        foreach ($iterator as $file) {
+            if ($file->isDir()) {
+                @mkdir($dst . '/' . $iterator->getSubPathName());
+            } else {
+                copy($file, $dst . '/' . $iterator->getSubPathName());
+            }
+        }
+    }
+}
