fc7438b7883ea665e333b9c122d8f975f6dbce2f
kenjis <kenji.uui@gmail.com>
2015-04-28 18:19:32 +0900

    Fix coding style

--- ci_instance.php
+++ ci_instance.php
@@ -36,8 +36,7 @@ if (file_exists(APPPATH . 'config/' . ENVIRONMENT . '/constants.php')) {
 $charset = strtoupper(config_item('charset'));
 ini_set('default_charset', $charset);
 
-if (extension_loaded('mbstring'))
-{
+if (extension_loaded('mbstring')) {
     define('MB_ENABLED', TRUE);
     // mbstring.internal_encoding is deprecated starting with PHP 5.6
     // and it's usage triggers E_DEPRECATED messages.
@@ -45,23 +44,18 @@ if (extension_loaded('mbstring'))
     // This is required for mb_convert_encoding() to strip invalid characters.
     // That's utilized by CI_Utf8, but it's also done for consistency with iconv.
     mb_substitute_character('none');
-}
-else
-{
+} else {
     define('MB_ENABLED', FALSE);
 }
 
 // There's an ICONV_IMPL constant, but the PHP manual says that using
 // iconv's predefined constants is "strongly discouraged".
-if (extension_loaded('iconv'))
-{
+if (extension_loaded('iconv')) {
     define('ICONV_ENABLED', TRUE);
     // iconv.internal_encoding is deprecated starting with PHP 5.6
     // and it's usage triggers E_DEPRECATED messages.
     @ini_set('iconv.internal_encoding', $charset);
-}
-else
-{
+} else {
     define('ICONV_ENABLED', FALSE);
 }
 
